6c63f69adc9b4dc9a3ad86202203c9d9e967edbb
24JR1A05Z9 <shaiksufiya847@gmail.com>
2026-02-28 14:12:21 +0530

    14.py

---
+++ 14.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========================== RESTART: D:/05z9/14.py ===========================
+enter length :67
+Inches: 201
+Yards: 3.35
+Miles: 0.01268939393939394
+Millimeter: 20421.600000000002
+Centimeter: 2042.16
+>>> 
